ad8181adfb490d4a27a56157763b839ffce64fe4
miguelamores <migueamores@gmail.com>
2015-09-17 22:22:48 -0500

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_many :measure
 	validates :name, :mail, :password, presence: true
 	validates :mail, uniqueness: true
-	validates :mail, confirmation: true
-	#validates :mail, :email_format 
+	#validates :mail, confirmation: true
+	validates :mail, :email_format => {:message => 'Invalid email_format'}
 	#validates_format_of :mail, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
