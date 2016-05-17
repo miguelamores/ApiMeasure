a3b1bd236cb91d5a82f3ca3abecafa221104cac8
miguelamores <migueamores@gmail.com>
2016-05-16 20:21:50 -0500

    measure_type field added

---
+++ db/migrate/20160517011606_add_measure_type_to_measures.rb
@@ -0,0 +1,5 @@
+class AddMeasureTypeToMeasures < ActiveRecord::Migration
+  def change
+  	add_column :measures, :measure_type, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151020015004) do
+ActiveRecord::Schema.define(version: 20160517011606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "measures", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
     t.decimal  "latitude"
     t.decimal  "longitude"
     t.decimal  "value"
     t.integer  "user_id"
+    t.text     "measure_type"
   end
 
   add_index "measures", ["user_id"], name: "index_measures_on_user_id", using: :btree
